919d7d0c7c971a8a39f4ca12a5d08d60ff2b5faf
Arina Goloubitskaya <arinaogo@gmail.com>
2025-12-12 22:53:33 +0300

    added config

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+
+# Thresholds for server load classification
+CPU_THRESHOLDS = {
+    'low': 20,
+    'high': 70
+}
+
+MEMORY_THRESHOLDS = {
+    'low': 30,
+    'high': 80
+}
+
+# LLM Configuration
+LLM_URL = os.getenv("LLM_URL", "http://llama-server:8080/completion")
+LLM_TIMEOUT = int(os.getenv("LLM_TIMEOUT", "90"))
+LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", "500"))
